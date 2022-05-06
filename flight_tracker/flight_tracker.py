4e93d5cdf29881b681610e64f663326042a7244b
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-05-06 21:02:19 +0100

    remove logger

--- flight_tracker/flight_tracker.py
+++ flight_tracker/flight_tracker.py
@@ -2,14 +2,11 @@ import time
 import sys
 from tkinter import messagebox
 
-from .utils.logger import Logger
 from .utils.env import is_development_environment
 from .results.results_writer import ResultsWriter
 from .simulator.flight import Flight, SimulatorConnectionError
 
 def main():
-    logger = Logger()
-
     try:
         flight = Flight()
     except SimulatorConnectionError:
@@ -21,9 +18,6 @@ def main():
 
     results_writer = ResultsWriter(flight.fieldnames)
 
-    logger.log(f"beginning flight tracking, output file is {results_writer.output_file_path}")
-
     while True:
-        logger.log("appending record")
         results_writer.append_record(flight.get_data())
         time.sleep(3 if is_development_environment() else 30)
--- flight_tracker/utils/logger.py
+++
@@ -1,9 +0,0 @@
-import datetime
-
-class Logger:
-    def log(self, msg):
-        print(f"[{datetime.datetime.now().isoformat()}] - {msg}")
-
-    def error(self, msg):
-        print(f"[{datetime.datetime.now().isoformat()}] - ERROR: {msg}")
-        
