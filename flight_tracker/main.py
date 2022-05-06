8e02c72ff172589d6fbbb129d82d062f77e550b9
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-05-06 13:44:46 +0100

    add results writer class

--- flight_tracker/main.py
+++ flight_tracker/main.py
@@ -1,38 +1,21 @@
 import time
-import os
-import csv
-import uuid
 
 import SimConnect
 
-docs_path = os.path.join(
-    os.path.expanduser("~/Documents"),
-    "Flight Tracker"
-)
-output_file_path = os.path.join(docs_path, f"{str(uuid.uuid4())}.csv")
+from .utils.results_writer import ResultsWriter
 
-print(f"beginning flight tracking, output file is {output_file_path}")
+results_writer = ResultsWriter()
+
+print(
+    f"beginning flight tracking, output file is {results_writer.output_file_path}"
+)
 
 sm = SimConnect.SimConnect()
 TWO_SECONDS = 2000
 aq = SimConnect.AircraftRequests(sm, _time=TWO_SECONDS)
 
-try:
-    os.mkdir(docs_path)
-except FileExistsError:
-    pass
-
 altitude = aq.find("PLANE_ALTITUDE")
 
-with open(output_file_path, "w", newline="", encoding="utf-8") as f:
-    fieldnames = ["altitude"]
-    writer = csv.DictWriter(f, fieldnames=fieldnames)
-    writer.writeheader()
-
 while True:
-    with open(output_file_path, "a", newline="", encoding="utf-8") as f:
-        fieldnames = ["altitude"]
-        writer = csv.DictWriter(f, fieldnames=fieldnames)
-        writer.writerow({"altitude": str(altitude.get())})
-
+    results_writer.append_record(altitude=str(altitude.get()))
     time.sleep(3)
---
+++ flight_tracker/utils/results_writer.py
@@ -0,0 +1,30 @@
+import os
+import csv
+import uuid
+
+class ResultsWriter:
+    def __init__(self):
+        self._fieldnames = ["altitude"]
+
+        docs_path = os.path.join(
+            os.path.expanduser("~/Documents"),
+            "Flight Tracker"
+        )
+
+        try:
+            os.mkdir(docs_path)
+        except FileExistsError:
+            pass
+
+        self.output_file_path = os.path.join(
+            docs_path, f"{str(uuid.uuid4())}.csv"
+        )
+
+        with open(self.output_file_path, "w", newline="", encoding="utf-8") as f:
+            writer = csv.DictWriter(f, fieldnames=self._fieldnames)
+            writer.writeheader()
+
+    def append_record(self, **record):
+        with open(self.output_file_path, "w", newline="", encoding="utf-8") as f:
+            writer = csv.DictWriter(f, fieldnames=self._fieldnames)
+            writer.writerow(record)
