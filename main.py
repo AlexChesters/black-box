050a4a32d7499264df43fc47de70d23a80a7e0cc
Alex Chesters <10558792+AlexChesters@users.noreply.github.com>
2022-05-06 12:45:16 +0100

    write to docs

--- main.py
+++ main.py
@@ -1,17 +1,38 @@
 import time
+import os
+import csv
+import uuid
 
 from SimConnect import *
 
-# Create SimConnect link
+docs_path = os.path.join(
+    os.path.expanduser("~/Documents"),
+    "Flight Tracker"
+)
+output_file_path = os.path.join(docs_path, f"{str(uuid.uuid4())}.csv")
+
+print(f"beginning flight tracking, output file is {output_file_path}")
+
 sm = SimConnect()
-# Note the default _time is 2000 to be refreshed every 2 seconds
-aq = AircraftRequests(sm, _time=2000)
+two_seconds = 2000
+aq = AircraftRequests(sm, _time=two_seconds)
+
+try:
+    os.mkdir(docs_path)
+except FileExistsError:
+    pass
 
 altitude = aq.find("PLANE_ALTITUDE")
 
-with open("foo.txt", "w") as f:
-    f.write(str(altitude.get()))
+with open(output_file_path, "w", newline="") as f:
+    fieldnames = ["altitude"]
+    writer = csv.DictWriter(f, fieldnames=fieldnames)
+    writer.writeheader()
 
 while True:
-    print(altitude.get())
+    with open(output_file_path, "a", newline="") as f:
+        fieldnames = ["altitude"]
+        writer = csv.DictWriter(f, fieldnames=fieldnames)
+        writer.writerow({"altitude": str(altitude.get())})
+
     time.sleep(3)
